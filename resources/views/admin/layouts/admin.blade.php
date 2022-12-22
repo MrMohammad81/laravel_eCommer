b9c23e225cf2b8b6a4f931815c49fc36f5c90f3f
Mr-Mohammad <mohammadhoseinmohit78@gmail.com>
2022-12-22 11:38:02 +0330

    admin panel --Dasboard implemented

---
+++ resources/views/admin/layouts/admin.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Webprog.ir - @yield('title')</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('admin/css/admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/sb-admin-2.css') }}" rel="stylesheet">
+
+    @yield('style')
+
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    @include('admin.sections.sidebar')
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            @include('admin.sections.topbar')
+            <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+
+                @yield('content')
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        @include('admin.sections.footer')
+        <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+@include('admin.sections.scroll_top')
+
+<!-- JavaScript-->
+<script defer src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+<script defer src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script defer src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script defer src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
+<script defer src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}"></script>
+<script defer src="{{ asset('admin/js/demo/chart-area-demo.js') }}"></script>
+<script defer src="{{ asset('admin/js/demo/chart-pie-demo.js') }}"></script>
+
+@yield('script')
+</body>
+
+</html>
